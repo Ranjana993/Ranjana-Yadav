43f6032b3d9ecb169475aa7983015db52bda85b2
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-07-13 21:03:23 +0530

    modified text color

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import "react-vertical-timeline-component/style.min.css";
@@ -9,13 +8,11 @@ export default function Experience() {
   return (
     <section id='work' className="scroll-mt-28 mb:12 lg:mb-28 sm:mb-40 ">
       <h1 className="text-center font-playfair text-5xl py-12 ">My experience</h1>
-      <VerticalTimeline lineColor="" 
+      <VerticalTimeline lineColor="white"
         className="vertical-timeline-element--work"
-        // contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        
       >
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
@@ -25,17 +22,17 @@ export default function Experience() {
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
-                color:"white",
+                color: "white",
                 padding: "1.3rem 2rem",
               }}
-              contentArrowStyle={{  borderRight: "0.4rem solid rgba(255, 255, 255, 0.5)" }}
+              contentArrowStyle={{ borderRight: "0.4rem solid rgba(255, 255, 255, 0.5)" }}
               date={item.date}
               icon={item.icon}
-              iconStyle={{ background: "rgba(255, 255, 255, 0.15)",fontSize: "1.5rem" }}
+              iconStyle={{ background: "rgba(255, 255, 255, 0.15)", fontSize: "1.5rem" }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
               <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700 ">{item.description}</p>
+              <p className="!mt-1 !font-normal  ">{item.description}</p>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
